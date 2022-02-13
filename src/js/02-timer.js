68eac3291a9e2759a6d787bf775665d9182359b8
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-13 16:03:10 +0200

    Start

---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    dateInput: document.querySelector('#datetime-picker')
+}
+
+console.log(refs.dateInput)
