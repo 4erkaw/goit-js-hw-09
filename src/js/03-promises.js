a2eb1bcc0bf092adcd5a54d107351c45c4decf48
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-13 21:50:25 +0200

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
   formField: document.querySelector('.form'),
-  // submitBtn: document.querySelector('button')
 }
 
-
-refs.formField.addEventListener('submit', createPromise)
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  
   return new Promise((resolve, reject) => {
-    if(shouldResolve){
-      resolve("stf")
-    }
-    reject("omg")
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if(shouldResolve){
+        resolve({position,delay})
+      }
+      reject({position,delay});
+    }, delay)
   })
 }
+
+function onSubmit(e){
+  e.preventDefault();
+  const form = e.currentTarget
+  const dataForm = new FormData(form);
+  const finalData = {};
+  for(const [key,value] of dataForm.entries()){
+    finalData[key] = Number(value);
+  }
+
+  form.reset();
+
+  for(let position = 1; position <= finalData.amount; position += 1){
+    createPromise(position,finalData.delay).then(success).catch(error);
+    finalData.delay = finalData.delay + finalData.step
+  }
+}
+
+function error({position,delay}){
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+function success({position,delay}){
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+}
+
+refs.formField.addEventListener('submit', onSubmit)
